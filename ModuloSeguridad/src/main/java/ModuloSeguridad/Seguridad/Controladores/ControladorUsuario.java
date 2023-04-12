dd734516b1332587ac16639d6f659f350a5bff5c
barrabas_dev <jacob.barrabas.dev@gmail.com>
2023-04-12 05:37:44 -0500

    se crea el api-gateway y se implementa el metodo login

--- ModuloSeguridad/src/main/java/ModuloSeguridad/Seguridad/Controladores/ControladorUsuario.java
+++ ModuloSeguridad/src/main/java/ModuloSeguridad/Seguridad/Controladores/ControladorUsuario.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 @CrossOrigin
@@ -99,5 +101,20 @@ public class ControladorUsuario {
             sb.append(String.format("%02x", b));
         }
         return sb.toString();
+    }
+
+    @PostMapping("/validar")
+    public Usuario validate(@RequestBody  Usuario infoUsuario,
+                            final HttpServletResponse response) throws IOException {
+        Usuario usuarioActual=this.miRepositorioUsuario
+                .getUserByEmail(infoUsuario.getCorreo());
+        if (usuarioActual!=null &&
+                usuarioActual.getContrasena().equals(convertirSHA256(infoUsuario.getContrasena()))) {
+            usuarioActual.setContrasena("");
+            return usuarioActual;
+        }else{
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return null;
+        }
     }
 }
--- ModuloSeguridad/src/main/java/ModuloSeguridad/Seguridad/Repositorios/RepositorioPermiso.java
+++ ModuloSeguridad/src/main/java/ModuloSeguridad/Seguridad/Repositorios/RepositorioPermiso.java
@@ -1,9 +1,12 @@
 
 package ModuloSeguridad.Seguridad.Repositorios;
 
+import org.springframework.data.mongodb.repository.Query;
 import ModuloSeguridad.Seguridad.Modelos.Permiso;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
 public interface RepositorioPermiso extends MongoRepository<Permiso,String> {
+    @Query("{'url':?0,'metodo':?1}")
+    Permiso getPermiso(String url, String metodo);
 }
